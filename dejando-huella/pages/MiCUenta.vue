cf0556196342b763b154d2d1852a48fc1bab0a83
Marco <marcomanzoruiz@gmail.com>
2024-06-06 09:37:49 -0600

    Se agregaron las páginas a las cuales el perfil del usuario lleva: MiCuenta, MisMascotas y AcercaDe. También se creó el menu del perfil del usuario.

---
+++ dejando-huella/pages/MiCUenta.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Mi Cuenta
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ dejando-huella/pages/MisMascotas.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Mis Mascotas
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
