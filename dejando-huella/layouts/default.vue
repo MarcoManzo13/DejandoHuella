79a5578d52e5e25ff751d11979e1fcd80eb4f7da
Marco <marcomanzoruiz@gmail.com>
2024-06-13 09:25:00 -0600

    Se hizo la App Bar responsiva

--- dejando-huella/layouts/default.vue
+++ dejando-huella/layouts/default.vue
@@ -2,41 +2,65 @@
     <div>
         <v-layout>
             <v-app-bar>
-                <v-app-bar-title style="text-align: left;">Dejando Huella</v-app-bar-title>
+                <template v-slot:prepend>
+                    <v-app-bar-nav-icon
+                        v-if="isMobile"
+                        variant="text"
+                        @click.stop="drawer = !drawer"
+                    />
+                    <v-app-bar-title>
+                        Dejando Huella
+                    </v-app-bar-title>
+                </template>
                 <template v-slot:append>
-                    <NuxtLink icon to="/">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
+                    <NuxtLink v-if="!isMobile" icon to="/">
+                        <v-btn style="color: #000;" variant="plain" class="mx-2" prepend-icon="mdi-home-outline">
                             Inicio
                         </v-btn>
                     </NuxtLink>
-                    <NuxtLink icon to="/Reporte">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
-                            Reportes
+                    <NuxtLink v-if="!isMobile" icon to="/Reporte">
+                        <v-btn style="color: #000;" variant="plain" class="mx-2" prepend-icon="mdi-emoticon-sad-outline">
+                            Perdidos
                         </v-btn>
                     </NuxtLink>
-                    <NuxtLink icon to="/Encontrados">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
-                            Encontrados
+                    <NuxtLink v-if="!isMobile" icon to="/Encontrados">
+                        <v-btn style="color: #000;" variant="plain" class="mx-2" prepend-icon="mdi-emoticon-happy-outline">
+                            Avistamientos
                         </v-btn>
                     </NuxtLink>
-                    <NuxtLink icon to="/Chat">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
+                    <NuxtLink v-if="!isMobile" icon to="/Chat">
+                        <v-btn style="color: #000;" variant="plain" class="mx-2" prepend-icon="mdi-chat-processing-outline">
                             Chat
                         </v-btn>
                     </NuxtLink>
-                    <NuxtLink icon to="/PerfilUsuario">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
-                            Perfil Usuario
-                        </v-btn>
-                    </NuxtLink>
-                    <NuxtLink icon to="/PerfilAdministrador">
-                        <v-btn style="color: #000;" variant="outlined" class="mx-2">
-                            Perfil Admin
+                    <NuxtLink v-if="!isMobile" icon to="/PerfilUsuario">
+                        <v-btn style="color: #000;" variant="plain" class="mx-2" prepend-icon="mdi-account-circle-outline">
+                            Perfil
                         </v-btn>
                     </NuxtLink>
                 </template>
             </v-app-bar>
-
+            <v-navigation-drawer
+                v-model="drawer"
+                temporary
+            >
+                <template class="d-flex my-3" elevation="0">
+                    <v-btn
+                        variant="text"
+                        @click="closeMenu"
+                    >
+                        <Icon name="mdi:close" size="2em"/>
+                    </v-btn>
+                </template>
+                <v-divider></v-divider>
+                <v-list dense nav>
+                    <template v-for="item in menuItems" :key="item.title">
+                        <NuxtLink :to="item.to" style="text-decoration: none; color: #000;">
+                            <v-list-item :prepend-icon="item.icon" :title="item.title" :value="item.value" active-class="activeLink"/>
+                        </NuxtLink>
+                    </template>
+                </v-list>
+            </v-navigation-drawer>
             <v-main>
                 <slot />
             </v-main>
@@ -44,11 +68,38 @@
     </div>
 </template>
 
-
 <script>
-
+    export default {
+        data() {
+            return {
+                drawer: false,
+                isMobile: false,
+                menuItems: [
+                    { to: "/", title: 'Inicio', icon: 'mdi-home-variant', value: 'inicio' },
+                    { to: "/Reporte", title: 'Reporte', icon: 'mdi-emoticon-sad-outline', value: 'reporte' },
+                    { to: "/Encontrados", title: 'Encontrados', icon: 'mdi-emoticon-happy-outline', value: 'encontrados' },
+                    { to: "/Chat", title: 'Chat', icon: 'mdi-chat-processing-outline', value: 'chat' },
+                    { to: "/PerfilUsuario", title: 'PerfilUsuario', icon: 'mdi-account-circle-outline', value: 'perfilUsuario' },
+                ],
+            }
+        },
+        mounted() {
+            this.checkIsMobile();
+            window.addEventListener('resize', this.checkIsMobile);
+        },
+        beforeDestroy() {
+            window.removeEventListener('resize', this.checkIsMobile);
+        },
+        methods: {
+            checkIsMobile() {
+                this.isMobile = window.innerWidth <= 1280;
+            },
+            closeMenu() {
+                this.drawer = !this.drawer;
+            },
+        },
+    }
 </script>
-
 
 <style scoped>
 </style>
